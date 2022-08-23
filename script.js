56a304284fb7f9381af84480ef34ef750ab956a1
karthigasun <prkarthik3@gmail.com>
2022-08-23 16:24:37 +0530

    dom-methods

--- script.js
+++ script.js
@@ -4,18 +4,13 @@ function validation(){
 
     if(name==null||name==""){
         alert("Please Enter Your Name");
+        document.getElementById("cname").innerHTML="Please Enter Your Name"
         return false;
     }
     else if(password.length<6){
-        alert("Make Strong Password");
+        alert("Make Strong Password ");
         return false;
     }
-
-    
-
-    // let email=document.getElementById("email").value;
-    // let a=email.indexof("@");
-    // let b=email.lastindexof(".")
 }
 function pass(){
     let repossword=document.getElementById("cpassword").value;
@@ -27,4 +22,42 @@ function pass(){
     else{
         document.getElementById("cpword").innerHTML="Password did't Match";
     }
+}
+
+function validate(){
+    let email=document.getElementById("email").value;
+    let at=email.indexOf("@");
+    let dot=email.lastIndexOf(".");
+
+    if(at<1|| dot<at+2 || dot+2>=email.length){
+        alert("Please Enter Valid Email Address");
+        return false;
+    }
+}
+
+function next(){
+    let name2=document.getElementById("name2").value;
+    let password2=document.getElementById("password2").value;
+    let status=false
+    if(name2==null || name2==""){
+        document.getElementById("cname2").innerHTML="Please Enter Your Name";
+        status=false;
+    }
+
+    else{
+        document.getElementById("cname2").innerHTML="Thank You";
+        status=true;
+    }
+
+    if(password2.length<6){
+        document.getElementById("cpword2").innerHTML="Make strong Password";
+        status=false;
+    }
+
+    else{
+        document.getElementById("cpword2").innerHTML="Visit Again";
+        status=true;
+    }
+    return status;
+
 }
